08a5abd3385d1d76dbc3202485fa44eb7d08b9c2
williamwlchan <william.wlchan@outlook.com>
2018-07-29 20:01:21 +0800

    20170729 Skip authencity token

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,13 +2,8 @@ class JobsController < ApplicationController
   def create
     job = Job.create(
       url: params[:url],
-      employer_name: params[:employer_name],
-      employer_description: params[:employer_description],
-      job_title: params[:job_title],
-      job_description: params[:job_description],
-      year_of_experience: params[:year_of_experience],
-      education_requirement: params[:education_requirement],
-      industry: params[:industry],
+      employer_name: params[:employer_name], employer_description: params[:employer_description], job_title: params[:job_title],
+      job_description: params[:job_description], year_of_experience: params[:year_of_experience], education_requirement: params[:education_requirement], industry: params[:industry],
       base_salary: params[:base_salary],
       employment_type_id: params[:employment_type_id]
     )
