7de4a4be32f7993613765621f28edaf5cda9090b
williamwlchan <william.wlchan@outlook.com>
2018-07-30 12:11:44 +0800

    20170730 Modify JobsController

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,6 @@
 class JobsController < ApplicationController
   def create
-    job = Job.create(
-      url: params[:url],
-      employer_name: params[:employer_name], employer_description: params[:employer_description], job_title: params[:job_title],
-      job_description: params[:job_description], year_of_experience: params[:year_of_experience], education_requirement: params[:education_requirement], industry: params[:industry],
-      base_salary: params[:base_salary],
-      employment_type_id: params[:employment_type_id]
-    )
+    job = Job.create(job_params)
     render json: { job: job }
   end
 
@@ -14,8 +8,14 @@ class JobsController < ApplicationController
     render json: Job.all
   end
 
-  def index_id
+  def show
     job = Job.find(params[:id])
     render json: { job: job }
   end
+
+  private
+
+    def job_params
+      params.require(:job).permit(:url, :employer_name, :employer_description, :job_title, :job_description, :year_of_experience, :education_requirement, :industry, :base_salary, :employment_type_id)
+    end
 end
